39ada3750ce6c9ae6286c278c63f6cb964d8170c
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-11-24 15:49:42 +0600

    added code

---
+++ 01_form-basic/hello.php
@@ -0,0 +1,13 @@
+<?php
+// $user = $_GET['username'];
+// $pass = $_GET['password'];
+
+$user = $_POST['username'];
+$pass = $_POST['password'];
+
+// $user = $_REQUEST['username'];
+// $pass = $_REQUEST['password'];
+
+echo $user;
+echo "<br>";
+echo $pass;
