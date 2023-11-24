81469d7cc07e1c07d6b0cfa73cbe99763fd228fc
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-11-24 11:09:39 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+    /* Name : Mostafizur Rahman
+       University : BUBT 
+       Age : 24
+    */
+
+    echo "Hello PHP !! ";
+    echo "Nice BD";
+
+    // comment
+    # comment
+
+
+
+?>
